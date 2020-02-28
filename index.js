081f69a037fcb7e35bada0ea4c1393f8df047839
MrBoombastic <mr.tolek2003@gmail.com>
2020-02-28 21:16:57 +0100

    Pierwsze wydanie

---
+++ index.js
@@ -0,0 +1,24 @@
+const run = async () => {
+    const fetch = require("node-fetch")
+    const parser = require('node-html-parser');
+    let hash = ""
+    let url = ""
+    async function findURL(input) {
+        const lines = input.split("\n");
+        for (let i = 0; i < lines.length; i++) {
+            if (lines[i].includes("var streamUrl =")) {
+                return lines[i].replace(/\s/g,'').replace('varstreamUrl=\'', '').replace(`?'.replace('.aac','.mp3');`, '').replace('.aac', '.mp3')
+            }
+        }
+    }
+    await fetch('http://www.eskago.pl/radio/vox-fm')
+        .then(res => res.text())
+        .then(body => {
+            hash = parser.parse(body).querySelector("#icsu").rawAttributes.value
+            url = await findURL(body)
+        });
+    const data = JSON.parse('{"' + decodeURI(hash).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}')
+    await console.log(data.hash, data.rip, data)
+console.log(`${url}?timestamp=${data.timestamp}&hash=${data.hash}&rip=${data.rip}&chstr=${data.chstr}`)
+}
+run()
