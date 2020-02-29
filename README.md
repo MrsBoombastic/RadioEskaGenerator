4d0251266c3302c066781ccf4f4a6cf2ac1e4180
MrBoombastic <mr.tolek2003@gmail.com>
2020-02-29 19:52:15 +0100

    wersja 1.1

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 Generowanie streamów do Eski. Bez ściemy i ukrywania kodu.
 
 # Użycie.
-W kodzie podmień domyślną stację (vox-fm) na inną, której nazwę można wyciągnąć wchodząc na http://www.eskago.pl/radio/. W przyszłości może się to zmeini, albo dostosuj sobie sam.
-Oczywiście, aby odpalić potrzebujesz Node.JS, a także zainstalować dodatkowe paczki komendą `npm install`. Nie tutaj będę uczył obsługi NPMa. Apkę odpala się przez `node index.js`. W konsoli po 2 sekundach pojawi się URL do słuchania. :D
+W kodzie podmień domyślną stację (vox-fm) na inną, której nazwę można wyciągnąć wchodząc na http://www.eskago.pl/radio/. W przyszłości może się to zmieni, ale obecnie to dostosuj sobie sam.
+Oczywiście, aby odpalić apkę potrzebujesz Node.JS, a także zainstalować dodatkowe paczki komendą `npm install`. Apkę odpala się przez `node index.js`. W konsoli po 2 sekundach pojawi się URL do słuchania. :D
 
 # Po co to komu?
-Aby zobaczyć jak Eska utrudnia ludziom życie.
+Aby zobaczyć jak Eska utrudnia uczciwym ludziom życie.
 
 # Kod jest beznadziejny!
 Owszem, jak najbardziej. Jeszcze jak!
 
 # O co chodzi z tą ściemą?
-Nie lubię, gdy autorzy rozwiązań publikują strony do generowania stacji radiowych, a nie mówią o tym JAK to robią. Tutaj każdy może zobaczyć jak to działa. Ponadto każdy kod, który napotkałem, był napisany w PHP, którego składnia wywołuje u mnie apopleksję.
+Nie lubię, gdy autorzy rozwiązań publikują strony do generowania stacji radiowych, a nie mówią o tym JAK to robią. Tutaj każdy może zobaczyć jak to działa. Ponadto każdy kod, który napotkałem, był napisany w PHP, którego składnia wywołuje u mnie apopleksję, więc kod napisano w JavaScript, który moim zdaniem jest czytelniejszy.
--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-const run = async () => {
-    const fetch = require("node-fetch")
-    const parser = require('node-html-parser');
-    let hash = ""
-    let url = ""
-    function findURL(input) {
-        const lines = input.split("\n");
-        for (let i = 0; i < lines.length; i++) {
-            if (lines[i].includes("var streamUrl =")) {
-                return lines[i].replace(/\s/g, '').replace('varstreamUrl=\'', '').replace(`?'.replace('.aac','.mp3');`, '').replace('.aac', '.mp3')
-            }
+const fetch = require("node-fetch")
+const parser = require('node-html-parser');
+
+function findURL(input) {
+    const lines = input.split("\n");
+    for (let i = 0; i < lines.length; i++) {
+        if (lines[i].includes("var streamUrl =")) {
+            return lines[i].replace(/\s/g, '').replace('varstreamUrl=\'', '').replace(`?'.replace('.aac','.mp3');`, '').replace('.aac', '.mp3')
         }
     }
-    await fetch('http://www.eskago.pl/radio/vox-fm')
-        .then(res => res.text())
-        .then(body => {
-            hash = parser.parse(body).querySelector("#icsu").rawAttributes.value
-            url = findURL(body)
-        });
-    const data = JSON.parse('{"' + decodeURI(hash).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}')
-    await console.log(data.hash, data.rip, data)
-console.log(`${url}?timestamp=${data.timestamp}&hash=${data.hash}&rip=${data.rip}&chstr=${data.chstr}`)
 }
-run()
+
+// Linijkę niżej podmienić można stację vox-fm na inną
+fetch('http://www.eskago.pl/radio/vox-fm')
+    .then(res => res.text())
+    .then(body => {
+        console.log(`${findURL(body)}?${parser.parse(body).querySelector("#icsu").rawAttributes.value}`)
+    });
